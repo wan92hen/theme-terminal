9268143f28ad90e73b538efb5c33df6757febd8a
wan92hen <wangzhen@fit2cloud.com>
2024-09-06 17:21:05 +0800

    feat: 支持显示文章目录

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "./styles/font-hack.scss";
 
 import Alpine from "alpinejs";
 import upvote from "./upvote";
+import * as tocbot from 'tocbot';
 
 window.Alpine = Alpine;
 
@@ -12,3 +13,33 @@ window.Alpine = Alpine;
 Alpine.data("upvote", upvote);
 
 Alpine.start();
+
+export function generateToc() {
+    const content = document.getElementById('content');
+    const titles = content?.querySelectorAll('h1, h2, h3, h4');
+    if (!titles || titles.length === 0) {
+      const tocContainer = document.querySelector('.toc-container');
+      tocContainer?.remove();
+      return;
+    }
+    tocbot.init({
+      tocSelector: '.toc',
+      contentSelector: '#content',
+      headingSelector: 'h1, h2, h3, h4',
+      extraListClasses: 'space-y-1 pl-2',
+      extraLinkClasses: 'p-1 text-sm hover:bg-[var(--green)] hover:text-[var(--white)]',
+      headingsOffset: 96,
+      scrollSmoothOffset: -96,
+      scrollSmooth: true,
+      // @ts-ignore
+      // tocScrollOffset: 50,
+    });
+  }
+export function locateToc() {
+  const content = document.getElementById('content');
+  const toc = document.querySelector('.toc') as HTMLElement;
+  let contentRight = content?.getBoundingClientRect().right;
+  if ( toc && contentRight) {
+    toc.style.left=`${contentRight}px`;
+  }
+}
