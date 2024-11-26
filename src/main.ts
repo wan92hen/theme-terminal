49d42e2295b26904108b0b56c1528e46de033c97
Ryan Wang <i@ryanc.cc>
2024-11-26 14:52:07 +0800

    chore: refactor vite config
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import "./styles/style.scss";
+import "./styles/font-hack.scss";
+import "./styles/font-pixel.scss";
 import Alpine from "alpinejs";
 import upvote from "./upvote";
-import * as tocbot from 'tocbot';
+import * as tocbot from "tocbot";
 
 window.Alpine = Alpine;
 
@@ -10,31 +13,32 @@ Alpine.data("upvote", upvote);
 Alpine.start();
 
 export function generateToc() {
-    const content = document.getElementById('content');
-    const titles = content?.querySelectorAll('h1, h2, h3, h4');
-    if (!titles || titles.length === 0) {
-      const tocContainer = document.querySelector('.toc-container');
-      tocContainer?.remove();
-      return;
-    }
-    tocbot.init({
-      tocSelector: '.toc',
-      contentSelector: '#content',
-      headingSelector: 'h1, h2, h3, h4',
-      extraListClasses: 'space-y-1 pl-2',
-      extraLinkClasses: 'p-1 text-sm hover:bg-[var(--green)] hover:text-[var(--white)]',
-      headingsOffset: 96,
-      scrollSmoothOffset: -96,
-      scrollSmooth: true,
-      // @ts-ignore
-      // tocScrollOffset: 50,
-    });
+  const content = document.getElementById("content");
+  const titles = content?.querySelectorAll("h1, h2, h3, h4");
+  if (!titles || titles.length === 0) {
+    const tocContainer = document.querySelector(".toc-container");
+    tocContainer?.remove();
+    return;
   }
+  tocbot.init({
+    tocSelector: ".toc",
+    contentSelector: "#content",
+    headingSelector: "h1, h2, h3, h4",
+    extraListClasses: "space-y-1 pl-2",
+    extraLinkClasses: "p-1 text-sm hover:bg-[var(--green)] hover:text-[var(--white)]",
+    headingsOffset: 96,
+    scrollSmoothOffset: -96,
+    scrollSmooth: true,
+    // @ts-ignore
+    // tocScrollOffset: 50,
+  });
+}
+
 export function locateToc() {
-  const content = document.getElementById('content');
-  const toc = document.querySelector('.toc') as HTMLElement;
-  let contentRight = content?.getBoundingClientRect().right;
-  if ( toc && contentRight) {
-    toc.style.left=`${contentRight}px`;
+  const content = document.getElementById("content");
+  const toc = document.querySelector(".toc") as HTMLElement;
+  const contentRight = content?.getBoundingClientRect().right;
+  if (toc && contentRight) {
+    toc.style.left = `${contentRight}px`;
   }
 }
--- src/upvote.ts
+++ src/upvote.ts
@@ -1,50 +1,50 @@
 interface upvoteState {
-    upvotedNames: string[];
-    init(): void;
-    upvoted(id: string): boolean;
-    handleUpvote(name: string): void;
-  }
-  
-  export default (key: string, group: string, plural: string): upvoteState => ({
-    upvotedNames: [],
-    init() {
-      this.upvotedNames = JSON.parse(localStorage.getItem(`walker.upvoted.${key}.names`) || "[]");
-    },
-    upvoted(id: string) {
-      return this.upvotedNames.includes(id);
-    },
-    async handleUpvote(name) {
-      if (this.upvoted(name)) {
+  upvotedNames: string[];
+  init(): void;
+  upvoted(id: string): boolean;
+  handleUpvote(name: string): void;
+}
+
+export default (key: string, group: string, plural: string): upvoteState => ({
+  upvotedNames: [],
+  init() {
+    this.upvotedNames = JSON.parse(localStorage.getItem(`walker.upvoted.${key}.names`) || "[]");
+  },
+  upvoted(id: string) {
+    return this.upvotedNames.includes(id);
+  },
+  async handleUpvote(name) {
+    if (this.upvoted(name)) {
+      return;
+    }
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "/apis/api.halo.run/v1alpha1/trackers/upvote");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onload = () => {
+      this.upvotedNames = [...this.upvotedNames, name];
+      localStorage.setItem(`walker.upvoted.${key}.names`, JSON.stringify(this.upvotedNames));
+
+      const upvoteNode = document.querySelector("[data-upvote-" + key + '-name="' + name + '"]');
+
+      if (!upvoteNode) {
         return;
       }
-  
-      const xhr = new XMLHttpRequest();
-  
-      xhr.open("POST", "/apis/api.halo.run/v1alpha1/trackers/upvote");
-      xhr.setRequestHeader("Content-Type", "application/json");
-  
-      xhr.onload = () => {
-        this.upvotedNames = [...this.upvotedNames, name];
-        localStorage.setItem(`walker.upvoted.${key}.names`, JSON.stringify(this.upvotedNames));
-  
-        const upvoteNode = document.querySelector("[data-upvote-" + key + '-name="' + name + '"]');
-  
-        if (!upvoteNode) {
-          return;
-        }
-  
-        const upvoteCount = parseInt(upvoteNode.textContent || "0");
-        upvoteNode.textContent = upvoteCount + 1 + "";
-      };
-      xhr.onerror = function () {
-        alert("网络请求失败，请稍后再试");
-      };
-      xhr.send(
-        JSON.stringify({
-          group: group,
-          plural: plural,
-          name: name,
-        })
-      );
-    },
-  });
+
+      const upvoteCount = parseInt(upvoteNode.textContent || "0");
+      upvoteNode.textContent = upvoteCount + 1 + "";
+    };
+    xhr.onerror = function () {
+      alert("网络请求失败，请稍后再试");
+    };
+    xhr.send(
+      JSON.stringify({
+        group: group,
+        plural: plural,
+        name: name,
+      })
+    );
+  },
+});
