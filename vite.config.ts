393d24b545c2ef74813cecdc67343b3d4e5881dc
Ryan Wang <i@ryanc.cc>
2024-11-26 15:46:38 +0800

    chore: refactor vite config (#21)
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,36 @@
+import PurgeIcons from "vite-plugin-purge-icons";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 import path from "path";
-import PurgeIcons from "vite-plugin-purge-icons";
 
-export default defineConfig({
-  base: "",
-  plugins: [PurgeIcons()],
-  build: {
-    outDir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "src/main.ts"),
-        style: path.resolve(__dirname, "src/styles/style.scss"),
-        fontHack: path.resolve(__dirname, "src/styles/font-hack.scss"),
-        fontPixel: path.resolve(__dirname, "src/styles/font-pixel.scss"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
-      },
+export default ({ mode }: { mode: string }) => {
+  const isProduction = mode === "production";
+
+  return defineConfig({
+    root: "./src",
+    base: isProduction ? "/themes/theme-terminal/assets/dist/" : "",
+    plugins: [PurgeIcons()],
+    define: {
+      "process.env": process.env,
     },
-  },
-});
+    build: {
+      manifest: isProduction,
+      minify: isProduction,
+      rollupOptions: {
+        input: path.resolve(__dirname, "src/main.ts"),
+        output: {
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name].js",
+          assetFileNames: "[name][extname]",
+        },
+        treeshake: false,
+        preserveEntrySignatures: "allow-extension",
+      },
+      outDir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
+      emptyOutDir: true,
+    },
+    server: {
+      origin: "http://localhost:5173",
+    },
+  });
+};
